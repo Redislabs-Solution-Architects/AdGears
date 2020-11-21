5db04f15f164ece596e13e539a038540d23e71d6
Chris Mague <github@mague.com>
2020-11-21 08:14:54 -0800

    collect scoring information as well

--- adgear.py
+++ adgear.py
@@ -1,12 +1,19 @@
 def findAd(name):
     demo = getDemo(name)
     c = "campaign:%s" %(demo)
-    ads = execute("zrevrange", c, 0, 100)
-    for ad in ads:
-        j = execute("BF.ADD", ad, name)
+    j = execute("zrevrange", c, 0, 100, "WITHSCORES")
+
+    # 0::2 in a list gets all the even numbered elements
+    ads    = j[0::2]
+    scores = j[1::2]
+
+    iters = 0
+    while iters < len(ads):
+        j = execute("BF.ADD", ads[iters], name)
         if j > 0:
-            return(ad)
-    return("FallBackAd")
+            return(ads[iters], scores[iters] )
+        iters += 1
+    return("FallBackAd", 0)
 
 def getDemo(name):
     u = "user:%s" %(name.replace(" ", ''))
